9c922a4867d164958b38c5c34ab6b3b0bbf1dda4
MinusGix <MinusGix@gmail.com>
2020-06-08 11:21:18 -0400

    Variety of changes

--- include/XyzClient.hpp
+++ include/XyzClient.hpp
@@ -46,61 +46,70 @@ namespace XyzCpp {
 			});
 		}
 
+		/// Returns whether the socket is still opened.
 		bool isConnected () {
 			return socket.is_open();
 		}
 
+		/// Close the socket.
 		void close () {
 			socket.close();
 		}
 
+		// These are templated because the function they're calling are templated. The templates could be more restrictive,
+		// but most (all?) of the time they're only being called internally.
+
+		/// Read data asynchronously from the socket with an arbitrary buffer & callback.
 		template<typename B, typename F>
 		void readAsync (B buffer, F callback) {
 			boost::asio::async_read(socket, buffer, callback);
 		}
 
+		/// Write data asynchronously to the socket with an arbitrary buffer & callback
 		template<typename B, typename F>
 		void writeAsync (B buffer, F callback) {
 			boost::asio::async_write(socket, buffer, callback);
 		}
 
+		/// Write data synchronously to the socket with an arbitrary buffer & callback
 		template<typename B>
 		void writeSync (B buffer) {
 			boost::asio::write(socket, buffer);
 		}
 	};
 
+	/// A TCP client for the XyzClient protocol. It would connect to a server that speaks this protocol.
 	class XyzClient {
 		public:
+		/// The # of bytes the length takes up. A uint32_t
 		static constexpr size_t length_size = 4;
+		/// The # of bytes the type takes up. A single byte.
+		/// (Though, the original source always converts it to an int..)
 		static constexpr size_t type_size = 1;
-
-		enum class State {
-			Length, // -> Type
-			Type, // -> Message
-			Message
-		};
 
 		protected:
 		TcpClient client;
 
-		size_t length = 0;
-		int type = 0;
-		std::vector<std::byte> message;
+		struct TempMessage {
+			/// Buffer for storing length and then type before turning them into integers.
+			std::array<std::byte, 4> buf;
+
+			size_t length = 0;
+			int type = 0;
+			std::vector<std::byte> message;
+		};
 
 		public:
-
+		
 		std::function<void(void)> onConnect;
 		std::function<void(void)> onDisconnect;
 		std::function<void(std::vector<std::byte>, int)> onMessage;
 
-		State state = State::Length;
-		std::vector<std::byte> temp_buffer;
-
+		/// Default constructor. Call [connect] to connect to a specific server.
+		/// Set onConnect, onDisconnect, and/or onMessage handlers
 		explicit XyzClient () {}
 		/// Note: this could cause issues, since it will fail/succeed to connect before you can apply the callbacks.
 		explicit XyzClient (std::string host, int port) {
-			assureBufferSize(64);
 			connect(host, port);
 		}
 		explicit XyzClient (TcpClient&& t_client) : client(std::move(t_client)) {}
@@ -113,14 +122,15 @@ namespace XyzCpp {
 
 		void disconnect (bool forced=false) {
 			if (forced || client.isConnected()) {
-				
 				client.close();
 				invokeOnDisconnect();
 			}
 		}
 
+		/// Connect to the [host] on [port].
+		/// Calls [onConnect] on success
+		/// Calls [onDisconnect] on any errors.
 		bool connect (std::string host, int port) {
-			std::cout << "Connecting\n";
 			try {
 				client.connect(host, port);
 
@@ -128,7 +138,7 @@ namespace XyzCpp {
 
 				read();
 				return true;
-			} catch (std::exception& err) { // TODO: this is bad, since we don't properly showcase the error, but it's what the original does
+			} catch (std::exception& err) { // TODO: Doesn't inform the user of any errors@He
 				invokeOnDisconnect();
 			}
 			return false;
@@ -162,6 +172,7 @@ namespace XyzCpp {
 			send(payload, type);
 		}
 
+		/// Send data synchronously
 		void send (const std::byte* data, size_t length, int type=0) {
 			try {
 				std::vector<std::byte> payload = XyzMessageBuilder()
@@ -170,8 +181,7 @@ namespace XyzCpp {
 
 				client.writeAsync(boost::asio::buffer(payload), [this] (boost::system::error_code ec, size_t length) {
 					if (ec) {
-						this->resetState();
-						this->invokeOnDisconnect();
+						this->handleError();
 					}
 				});
 			} catch (...) {} // TODO: handle error?
@@ -200,6 +210,7 @@ namespace XyzCpp {
 			sendSync(payload, type);
 		}
 
+		/// Send data synchronously as a message (compressed)
 		void sendSync (const std::byte* data, size_t length, int type=0) {
 			try {
 				std::vector<std::byte> payload = XyzMessageBuilder()
@@ -214,81 +225,86 @@ namespace XyzCpp {
 		private:
 
 		void readLength () {
-			state = State::Length;
-			assureBufferSize(length_size);
-			client.readAsync(boost::asio::buffer(temp_buffer.data(), length_size), [this] (boost::system::error_code ec, size_t length) {
-				this->receivedLength(ec, length);
+			/// Note: this has to be shared instead of merely a unique ptr because we would then want to move it into the lambda to extend it's lifetime
+			/// but then our lambda has move-only data in it, which makes it non-copyable, and internally the boost uses std::function (or something similar)
+			/// which requires the lambda to be Copyable.
+			std::shared_ptr<TempMessage> temp_message = std::make_shared<TempMessage>();
+			client.readAsync(boost::asio::buffer(temp_message->buf.data(), length_size), [this, temp_message] (boost::system::error_code ec, size_t received_length) {
+				this->receivedLength(ec, received_length, temp_message);
 			});
 		}
 
-		void receivedLength (boost::system::error_code ec, size_t) {
+		void receivedLength (boost::system::error_code ec, size_t received_length, std::shared_ptr<TempMessage> temp_message) {
 			if (ec) {
-				handleReceiveError();
+				handleError();
 				return;
 			}
 
 			try {
-				// todo: throw error if there's not enough space in message (check what BitConverter.ToInt32 would throw)
-				length = XyzUtils::detail::asU32(temp_buffer.at(3), temp_buffer.at(2), temp_buffer.at(1), temp_buffer.at(0));
+				// Fail if there's not enough bytes for a four byte integer (BitConverter.toInt32 does this)
+				// (Though, we're using an array, so it's always filled, so we used the received_length variable to know what values are valid)
+				if (received_length < 4) {
+					throw std::runtime_error("Failed to convert to int32, not enough bytes");
+				}
 
-				state = State::Type;
-				assureBufferSize(type_size);
-				client.readAsync(boost::asio::buffer(temp_buffer.data(), type_size), [this] (boost::system::error_code ec, size_t length) {
-					this->receivedType(ec, length);
+				// Convert bytes read into temp buffer into a uint32_t
+				std::array<std::byte, 4>& buf = temp_message->buf;
+				temp_message->length = XyzUtils::detail::asU32(buf.at(3), buf.at(2), buf.at(1), buf.at(0));
+
+				// Resize the buffer to be able to contain the message
+				temp_message->message.resize(temp_message->length);
+
+				// Ask for the type byte
+				client.readAsync(boost::asio::buffer(buf.data(), type_size), [this, temp_message] (boost::system::error_code ec, size_t t_received_length) {
+					this->receivedType(ec, t_received_length, temp_message);
 				});
 			} catch (...) { // TODO: handle error properly?
-				handleReceiveError();
+				handleError();
 			}
 		}
 
-		void receivedType (boost::system::error_code ec, size_t) {
+		void receivedType (boost::system::error_code ec, size_t received_length, std::shared_ptr<TempMessage> temp_message) {
 			if (ec) {
-				handleReceiveError();
+				handleError();
 				return;
 			}
 
 			try {
+				// Fail if not enough bytes for the byte of [type]
+				if (received_length < 1) {
+					throw std::runtime_error("Failed to convert to byte, no bytes");
+				}
+
 				// TODO: throw error if it can't acccess
-				type = static_cast<int>(temp_buffer.at(0));
+				temp_message->type = static_cast<int>(temp_message->buf.at(0));
 
-				state = State::Message;
-				assureBufferSize(length);
-				client.readAsync(boost::asio::buffer(temp_buffer.data(), length), [this] (boost::system::error_code ec, size_t length) {
-					this->receivedMessage(ec, length);
+				client.readAsync(boost::asio::buffer(temp_message->message.data(), temp_message->length), [this, temp_message] (boost::system::error_code ec, size_t t_received_length) {
+					this->receivedMessage(ec, t_received_length, temp_message);
 				});
 			} catch (...) { // TODO: handle error properly?
-				handleReceiveError();
+				handleError();
 			}
 		}
 
-		void receivedMessage (boost::system::error_code ec, size_t) {
+		void receivedMessage (boost::system::error_code ec, size_t received_length, std::shared_ptr<TempMessage> temp_message) {
 			if (ec) {
-				handleReceiveError();
+				handleError();
 				return;
 			}
 
 			try {
-				message = XyzUtils::inflate(temp_buffer.data(), temp_buffer.size());
-				invokeOnMessage(std::move(message), type);
+				// the deflated message is contained within message
+				std::vector<std::byte> deflated = XyzUtils::inflate(temp_message->message);
+				invokeOnMessage(std::move(deflated), temp_message->type);
 
 				readLength();
 			} catch (...) { // TODO: handle error properly?
-				handleReceiveError();
+				handleError();
 			}
 		}
 
-		void handleReceiveError () {
-			resetState();
+		void handleError () {
 			invokeOnDisconnect();
-		}
-
-		/// Makes sure the buffer has enough space to hold [length]
-		void assureBufferSize (size_t length) {
-			temp_buffer.resize(length);
-		}
-
-		void resetState () {
-			state = State::Length;
 		}
 
 		void invokeOnConnect () {
